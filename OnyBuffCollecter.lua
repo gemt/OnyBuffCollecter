c8c1883594e324b4dca82f0bf189d3dae75bf761
Kristian Skarseth <skarseth@mixedrealities.no>
2016-03-30 18:59:54 +0200

    tested and working identification of the correct yell, timestamp and name of player who used it

--- OnyBuffCollecter.lua
+++ OnyBuffCollecter.lua
@@ -1,22 +1,42 @@
 local Frame = CreateFrame("FRAME");
-Frame:RegisterEvent("CHAT_MSG_YELL");
-Frame:SetScript("OnEvent", function () ChatParse_EventHandler(event, arg1, arg2) end)
+--Frame:RegisterEvent("CHAT_MSG_YELL");
+Frame:RegisterEvent("CHAT_MSG_MONSTER_EMOTE");
+Frame:RegisterEvent("CHAT_MSG_MONSTER_PARTY");
+Frame:RegisterEvent("CHAT_MSG_MONSTER_SAY");
+Frame:RegisterEvent("CHAT_MSG_MONSTER_WHISPER");
+Frame:RegisterEvent("CHAT_MSG_MONSTER_YELL");
+
+Frame:SetScript("OnEvent", function () OnyBuffCollecter_EventHandler(event, arg1, arg2) end)
 
 local start_str = "People of the Horde, citizens of Orgrimmar, come, gather round and celebrate heroes of the Horde. On this day, "
-local end_str = ", under the auspicies of our glorious Warchief, laid a mortal blow against the Black Dragonflight. The brood mother. Onyxia, has been slain!"
-function ChatParse_EventHandler(event, text, player)
+local end_str = ", under the auspices of our glorious Warchief, laid a mortal blow against the Black Dragonflight. The brood mother, Onyxia, has been slain!"
 
-	if event != "CHAT_MSG_YELL" then return
+function OnyBuffCollecter_EventHandler(event, text, player)
+	_print(event)
+	if player ~= "Overlord Runthak" then
+		_print("not overlord, returning")
+		return
+	end
 	
-	if player != "Overlord Runthak" then return
-	
-	_print("Got yell from Overlord Runthak!")
-	_print(text)
+	local first10 = string.sub(text,1,6)
+	_print(first10)
+	if first10 ~= "People" then 
+		_print("notmatch")
+		return 
+	end
 	
 	local without_start = string.gsub(text,start_str, "")
 	local without_all = string.gsub(without_start, end_str, "")
-	_print(without_all)
 	
+	_print("wo all: "..without_all)
+	_print("time: "..GetTime())
+	local hours,minutes=GetGameTime()
+	_print(hours..":"..minutes)
+	_print(without_all.." used ony buff at "..hours..":"..minutes)
+end
+
+function ony_buff_collector_test()
+	_print("test success")
 end
 
 --Prints a message in the default chatframe.
